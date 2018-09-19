7c54774cc6805408f69d82334ac04d60adb015a5
seungmun <tjdans174@gmail.com>
2018-09-19 17:43:09 +0900

    [#10] Update : Notice테이블과 양방향 관계를 만들어주기 위해 추가

--- src/main/java/com/sungmun/NoticeBoard/domain/user/User.java
+++ src/main/java/com/sungmun/NoticeBoard/domain/user/User.java
@@ -1,15 +1,16 @@
 package com.sungmun.NoticeBoard.domain.user;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
 import org.springframework.data.annotation.Id;
 
-import com.sungmun.NoticeBoard.service.LocalDatePersistenceConverter;
+import com.sungmun.NoticeBoard.domain.notice.Notice;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -30,14 +31,19 @@ public class User {
 	private String firstName;
 	@Column(name="user_secondname",length = 10, nullable = false)
 	private String secondName;
+	
 	@Column(name="joindate")
 	private LocalDateTime joindate;
+	
 	@Column(name="user_phone",length = 12, nullable = false)
 	private String phone;
 	@Column(name="user_email",length = 50, nullable = false)
 	private String email;
 	@Column(name="user_image",length=150)
 	private String image;
+	
+	@OneToMany(mappedBy="Notice")
+	private List<Notice> Notices=new ArrayList<>();
 	
 	@Builder
 	public User(String id, String password, String firstName, String secondName, LocalDateTime joindate, String phone, String email,String image) {
