2e9a613134eb9253e4c8858896f7ccd84b4d1c35
seungmun <tjdans174@gmail.com>
2018-09-20 17:33:51 +0900

    [#10] Update : 잘못된 import 수정

--- src/main/java/com/sungmun/NoticeBoard/domain/notice/Notice.java
+++ src/main/java/com/sungmun/NoticeBoard/domain/notice/Notice.java
@@ -2,13 +2,15 @@ package com.sungmun.NoticeBoard.domain.notice;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import org.springframework.data.annotation.Id;
 
 import com.sungmun.NoticeBoard.domain.user.User;
 
--- src/main/java/com/sungmun/NoticeBoard/domain/user/User.java
+++ src/main/java/com/sungmun/NoticeBoard/domain/user/User.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
-import org.springframework.data.annotation.Id;
 
 import com.sungmun.NoticeBoard.domain.notice.Notice;
 
