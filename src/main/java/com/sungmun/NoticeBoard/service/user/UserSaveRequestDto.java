df96327ad0e9553b5b34e2df3f9c5253c6f006d2
seungmun <tjdans174@gmail.com>
2018-09-20 19:55:30 +0900

    [#12] Fead : User데이터를 저장하는 DTO클래스

---
+++ src/main/java/com/sungmun/NoticeBoard/service/user/UserSaveRequestDto.java
@@ -0,0 +1,31 @@
+package com.sungmun.NoticeBoard.service.user;
+
+import com.sungmun.NoticeBoard.domain.user.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserSaveRequestDto {
+	private String id;
+	private String password;
+	private String firstName;
+	private String secondName;
+	private String phone;
+	private String email;
+	
+	
+	public User toEntity() {
+		return User.builder()
+				.id(id)
+				.password(password)
+				.firstName(firstName)
+				.secondName(secondName)
+				.phone(phone)
+				.email(email)
+				.build();
+	}
+}
