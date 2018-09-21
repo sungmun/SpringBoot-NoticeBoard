0e5942af43f4ada416392a6ab8e7edbb4e9bf3ed
seungmun <tjdans174@gmail.com>
2018-09-21 19:44:12 +0900

    [#14] Fead :  리스트를 호출하기 위한 Dto클래스로 날짜데이터를 변환시켜서 저장한다

---
+++ src/main/java/com/sungmun/NoticeBoard/dto/notice/NoticeMainResponseDto.java
@@ -0,0 +1,30 @@
+package com.sungmun.NoticeBoard.dto.notice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+import com.sungmun.NoticeBoard.domain.notice.Notice;
+
+import lombok.Getter;
+
+@Getter
+public class NoticeMainResponseDto {
+	private Long num;
+	private String title;
+	private String user;
+	private String createDate;
+	
+	public NoticeMainResponseDto(Notice entity) {
+		num=entity.getNum();
+		title=entity.getTitle();
+		user=entity.getUser().getId();
+		createDate=toStringDateTime(entity.getCreateDate());
+	}
+	private String toStringDateTime(LocalDateTime dateTime) {
+		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-mm-dd HH:mm:ss");
+		return Optional.ofNullable(dateTime)
+				.map(formatter::format)
+				.orElse("");
+	}
+}
