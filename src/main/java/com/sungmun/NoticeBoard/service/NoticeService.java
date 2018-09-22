63ba054441da271160fd9a1204bf8d49d1f8bad6
seungmun <tjdans174@gmail.com>
2018-09-22 22:28:38 +0900

    [#14] Update :  페이지 처리시에 필요한 인자인 Pageable를 추가해줌

--- src/main/java/com/sungmun/NoticeBoard/service/NoticeService.java
+++ src/main/java/com/sungmun/NoticeBoard/service/NoticeService.java
@@ -4,6 +4,8 @@ package com.sungmun.NoticeBoard.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,8 +20,8 @@ public class NoticeService {
 	private NoticeRepository noticeRepository;
 	
 	@Transactional(readOnly=true)
-	public List<NoticeMainResponseDto> findAll(){
-		return noticeRepository.findAll()
+	public List<NoticeMainResponseDto> findAll(Pageable pageable){
+		return noticeRepository.findAll(pageable)
 				.stream()
 				.map(NoticeMainResponseDto::new)
 				.collect(Collectors.toList());
