de534a52289f77d671f2c2cf6625afc7c64571df
seungmun <tjdans174@gmail.com>
2018-09-23 17:24:43 +0900

    [#14] Update : 페이지 이동 링크를 제거하고 현제 페이지의 링크에 맞는 페이지 이동 링크를 생성해주는 함수 생성

--- src/main/resources/static/js/app/IndexApp.js
+++ src/main/resources/static/js/app/IndexApp.js
@@ -15,6 +15,21 @@ var index={
 		}
 		
 		return range;
+	},
+	pageChangeNum:function(){
+		var page=$('ul');
+		var nowpage=page.data('nowpage')+1;
+		
+		$('ul li').remove();
+		
+		this.pageRange(nowpage,page.data('totalpage')).forEach(function(value,index){
+			var li=$('<li/>');
+			
+			if(value==nowpage)	li.addClass('is-active');
+			li.append($('<a/>',{text:value}));
+			
+			page.append(li);
+		});
 	}
 };
 index.init();
