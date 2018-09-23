76765418f87bdddb79b1a96f19c37bd748e51cc9
seungmun <tjdans174@gmail.com>
2018-09-23 17:20:32 +0900

    [#14] Update : 현제 페이지와 전체 페이지의 개수를 가지고 현재 페이지의 범위를 계산해주는 함수 생성

--- src/main/resources/static/js/app/IndexApp.js
+++ src/main/resources/static/js/app/IndexApp.js
@@ -1,7 +1,20 @@
 var index={
 	init:function(){
 		var _this=this;
+	},
+	pageRange:function(nowpage,totalpage){
+		var minpage=nowpage-4;
+		var maxpage=nowpage+4;
 		
+		if(minpage<=0) minpage=1;
+		if(maxpage>=totalpage) maxpage=totalpage;
+		
+		var range=[];
+		for(;minpage<=maxpage;minpage++){
+			range.push(minpage);
+		}
+		
+		return range;
 	}
 };
 index.init();
