e52790b3af85d24623690cc48db2f90a2969de87
seungmun <tjdans174@gmail.com>
2018-09-27 16:55:16 +0900

    [#21] Update : 중복되는 함수를 제거후 BaseTimeDto클래스를 상속받아 생성된 날짜를 저장

--- src/main/java/com/sungmun/NoticeBoard/dto/notice/NoticeMainResponseDto.java
+++ src/main/java/com/sungmun/NoticeBoard/dto/notice/NoticeMainResponseDto.java
@@ -1,30 +1,20 @@
 package com.sungmun.NoticeBoard.dto.notice;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Optional;
-
 import com.sungmun.NoticeBoard.domain.notice.Notice;
+import com.sungmun.NoticeBoard.dto.BaseTimeDto;
 
 import lombok.Getter;
 
 @Getter
-public class NoticeMainResponseDto {
+public class NoticeMainResponseDto extends BaseTimeDto{
 	private Long num;
 	private String title;
 	private String user;
-	private String createDate;
 	
 	public NoticeMainResponseDto(Notice entity) {
+		super(entity.getCreateDate());
 		num=entity.getNum();
 		title=entity.getTitle();
 		user=entity.getUser().getId();
-		createDate=toStringDateTime(entity.getCreateDate());
-	}
-	private String toStringDateTime(LocalDateTime dateTime) {
-		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-mm-dd");
-		return Optional.ofNullable(dateTime)
-				.map(formatter::format)
-				.orElse("");
 	}
 }
