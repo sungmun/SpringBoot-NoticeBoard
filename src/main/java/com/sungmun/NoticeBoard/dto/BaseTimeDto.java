3bb15f6f07fb3210d4e696f9744af95454b3332d
seungmun <tjdans174@gmail.com>
2018-09-29 16:15:46 +0900

    [#21] Update : 시간관련 Dto에서 필드를 제거하고 정적메소드로 변경을 하였으며, 문자열을 날짜형식으로 바꾸는 메소드와 날짜 형식을 문자열로 바꾸는 메소드를 지정하였다

--- src/main/java/com/sungmun/NoticeBoard/dto/BaseTimeDto.java
+++ src/main/java/com/sungmun/NoticeBoard/dto/BaseTimeDto.java
@@ -5,11 +5,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 public class BaseTimeDto {
-	protected String createDate;
-	public BaseTimeDto(LocalDateTime dateTime) {
-		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-mm-dd");
-		createDate= Optional.ofNullable(dateTime)
-				.map(formatter::format)
-				.orElse("");
+	public static String toStringLocalDateTime(LocalDateTime dateTime, String pattern) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);//yyyy-MM-dd
+		return Optional.ofNullable(dateTime).map(formatter::format).orElse("");
+	}
+
+	public static LocalDateTime toLocalDateTimeString(String dateTime, String pattern) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);//yyyy-MM-dd
+		return LocalDateTime.parse(dateTime, formatter);
 	}
 }
