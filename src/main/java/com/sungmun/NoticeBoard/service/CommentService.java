f7b6ba87d97e47a4a94b62e430423ea133490350
seungmun <tjdans174@gmail.com>
2018-10-06 21:17:55 +0900

    [#21] Update : 어노테이션 추가

--- src/main/java/com/sungmun/NoticeBoard/service/CommentService.java
+++ src/main/java/com/sungmun/NoticeBoard/service/CommentService.java
@@ -3,11 +3,20 @@ package com.sungmun.NoticeBoard.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.sungmun.NoticeBoard.domain.comment.CommentRepository;
 import com.sungmun.NoticeBoard.dto.comment.CommentReadResponseDto;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
 public class CommentService {
 	private CommentRepository repository;
+	
+	@Transactional(readOnly=true)
 	public List<CommentReadResponseDto> findByid(final long noticeId){
 		return repository.findAll()
 				.stream()
