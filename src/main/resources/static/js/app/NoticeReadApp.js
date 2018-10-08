f6dc943696379795477f8cc8f1c87bc51ca6d4e2
seungmun <tjdans174@gmail.com>
2018-10-08 18:25:07 +0900

    [#21] Update : Comment 추가를 담당하는 메소드 추가

--- src/main/resources/static/js/app/NoticeReadApp.js
+++ src/main/resources/static/js/app/NoticeReadApp.js
@@ -1,6 +1,23 @@
 var read={
-		init:function(){
-			var _this=this;
-		}
+	init:function(){
+		var _this=this;
+	},
+	commentRead:function(){
+		var data={
+			num:$('.notice').data('notice')
+		};
+		
+		$.ajax({
+			url:'/comment/read',
+			type:'get',
+			data: data,
+			dataType:'json',
+			contentType:'application/json; charset=utf-8',
+		}).done(function(data){
+			console.log(data);
+		}).fail(function(error){
+			console.log(error);
+		});
+	},
 }
 read.init();
