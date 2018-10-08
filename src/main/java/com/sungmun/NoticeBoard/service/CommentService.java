5e8818b1527488659b4a288e63fb6dfcb293556a
seungmun <tjdans174@gmail.com>
2018-10-08 18:24:16 +0900

    [#21] Update : Comment 추가를 담당하는 메소드 추가

--- src/main/java/com/sungmun/NoticeBoard/service/CommentService.java
+++ src/main/java/com/sungmun/NoticeBoard/service/CommentService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.sungmun.NoticeBoard.domain.comment.CommentRepository;
 import com.sungmun.NoticeBoard.dto.comment.CommentReadResponseDto;
+import com.sungmun.NoticeBoard.dto.comment.CommentSaveRequestDto;
 
 import lombok.AllArgsConstructor;
 
@@ -15,13 +16,16 @@ import lombok.AllArgsConstructor;
 @Service
 public class CommentService {
 	private CommentRepository repository;
-	
-	@Transactional(readOnly=true)
-	public List<CommentReadResponseDto> findByid(final long noticeId){
+
+	@Transactional(readOnly = true)
+	public List<CommentReadResponseDto> findByid(final long noticeId) {
 		return repository.findAll()
 				.stream()
-				.filter(comment->(comment.getNotice().getNum()==noticeId))
-				.map(CommentReadResponseDto::new)
-				.collect(Collectors.toList());
+				.filter(comment -> (comment.getNotice().getNum() == noticeId))
+				.map(CommentReadResponseDto::new).collect(Collectors.toList());
+	}
+
+	public Long save(final CommentSaveRequestDto dto) {
+		return repository.save(dto.toEntity()).getNum();
 	}
 }
