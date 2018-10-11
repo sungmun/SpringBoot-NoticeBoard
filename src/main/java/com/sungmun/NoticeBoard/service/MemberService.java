9695839922997445ed60d7591795a88137cf9bfa
seungmun <tjdans174@gmail.com>
2018-10-11 16:34:16 +0900

    [#16] Fead : 멤버의 서비스 관련 로직을 넣을 클래스 생성

---
+++ src/main/java/com/sungmun/NoticeBoard/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.sungmun.NoticeBoard.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class MemberService {
+	
+}
