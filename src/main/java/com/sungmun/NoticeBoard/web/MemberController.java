1132cd5e951a3c77461262e4f991aef06b34235d
seungmun <tjdans174@gmail.com>
2018-10-11 16:36:46 +0900

    [#16] Update : 회원가입에 대한 컨트롤 메소드 추가

--- src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
+++ src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
@@ -1,7 +1,13 @@
 package com.sungmun.NoticeBoard.web;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sungmun.NoticeBoard.dto.member.MemberSaveRequestDto;
+import com.sungmun.NoticeBoard.service.MemberService;
 
 import lombok.AllArgsConstructor;
 
@@ -9,5 +15,13 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+	MemberService service;
 	
+	@PostMapping("/create")
+	public String create(@RequestBody MemberSaveRequestDto dto) {
+		BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+		dto.setPassword(passwordEncoder.encode(dto.getPassword()));
+		service.save(dto);
+		return "redirect:/";
+	}
 }
