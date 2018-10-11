a1a0ab98184bf49b6dfd4cb866377e1c04ea3c9b
seungmun <tjdans174@gmail.com>
2018-10-11 16:31:49 +0900

    [#16] Fead : 멤버를 컨트롤하는 클래스 생성

---
+++ src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
@@ -0,0 +1,13 @@
+package com.sungmun.NoticeBoard.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+	
+}
