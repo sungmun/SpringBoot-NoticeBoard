771e7634adbc585d411e4b9bd42c96cea854e2f9
seungmun <tjdans174@gmail.com>
2018-10-11 16:36:27 +0900

    [#16] Update : 회원가입에 대한 서비스 로직 추가

--- src/main/java/com/sungmun/NoticeBoard/service/MemberService.java
+++ src/main/java/com/sungmun/NoticeBoard/service/MemberService.java
@@ -2,10 +2,17 @@ package com.sungmun.NoticeBoard.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sungmun.NoticeBoard.domain.member.MemberRepository;
+import com.sungmun.NoticeBoard.dto.member.MemberSaveRequestDto;
+
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
 public class MemberService {
+	private MemberRepository repository;
 	
+	public String save(MemberSaveRequestDto dto) {
+		return repository.save(dto.toEntity()).getId();
+	}
 }
