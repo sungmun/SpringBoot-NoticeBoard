1d40c3a316666bfdfb5af5284d605fd08866d1a8
seungmun <tjdans174@gmail.com>
2018-10-12 18:42:33 +0900

    [#16] Update : h2 DB Console에 접속시 막히는 경우를 수정

--- src/main/java/com/sungmun/NoticeBoard/SecurityConfig.java
+++ src/main/java/com/sungmun/NoticeBoard/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,8 +14,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/admin/**").hasRole("ADMIN")
+		http
+		.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/**").permitAll();
+		
+		http.authorizeRequests().antMatchers("/h2-console/*").permitAll();
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
 	}
 }
