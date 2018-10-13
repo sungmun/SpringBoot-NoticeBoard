8ff4e9925edbaafeeab607932a582861ca339841
seungmun <tjdans174@gmail.com>
2018-10-13 20:44:09 +0900

    [#15] Fead : Security에 login페이지 지정

--- src/main/java/com/sungmun/NoticeBoard/SecurityConfig.java
+++ src/main/java/com/sungmun/NoticeBoard/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,7 +15,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
-		.antMatchers("/**").permitAll();
+		.antMatchers("/**").permitAll()
+		.and()
+		.formLogin().loginPage("/login").defaultSuccessUrl("/").usernameParameter("id").passwordParameter("password");
 		
 		http.authorizeRequests().antMatchers("/h2-console/*").permitAll();
 		http.csrf().disable();
