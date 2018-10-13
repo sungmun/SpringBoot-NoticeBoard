78f1b0077983d96a3934e45ddd787d01622553a5
seungmun <tjdans174@gmail.com>
2018-10-13 20:42:13 +0900

    [#15] Fead : 회원가입 이나 로그인시 url연결

--- src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
+++ src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
@@ -1,11 +1,9 @@
 package com.sungmun.NoticeBoard.web;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sungmun.NoticeBoard.dto.member.MemberSaveRequestDto;
 import com.sungmun.NoticeBoard.service.MemberService;
@@ -14,11 +12,20 @@ import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/member")
 public class MemberController {
 	MemberService service;
-	
-	@PostMapping("/create")
+
+	@GetMapping("/login")
+	public String login() {
+		return "member/login";
+	}
+
+	@GetMapping("/register")
+	public String register() {
+		return "member/register";
+	}
+
+	@PostMapping("/member/create")
 	public String create(@RequestBody MemberSaveRequestDto dto) {
 		service.save(dto);
 		return "redirect:/";
