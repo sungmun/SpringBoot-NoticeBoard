a0d2617d47a7e5fe24cb50d04e6decf2c45c1e29
seungmun <tjdans174@gmail.com>
2018-10-15 20:46:13 +0900

    [#15] Fead : 생성자 인자로 Member을 받으며, 받은 인자를 통해서 클래스를 구성한다

--- src/main/java/com/sungmun/NoticeBoard/dto/member/SecurityMember.java
+++ src/main/java/com/sungmun/NoticeBoard/dto/member/SecurityMember.java
@@ -1,17 +1,26 @@
 package com.sungmun.NoticeBoard.dto.member;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-public class SecurityMember extends User {
+import com.sungmun.NoticeBoard.domain.member.Member;
+import com.sungmun.NoticeBoard.domain.member.MemberRole;
 
-	public SecurityMember(String username, String password, boolean enabled, boolean accountNonExpired,
-			boolean credentialsNonExpired, boolean accountNonLocked,
-			Collection<? extends GrantedAuthority> authorities) {
-		super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
-		// TODO Auto-generated constructor stub
+public class SecurityMember extends User {
+	private static final String ROLE_PREFIX = "ROLE_";
+	private static final long serialVersionUID = 1L;
+
+	public SecurityMember(Member member) {
+		super(member.getId(),member.getPassword(),makeGrantedAuthority(member.getRoles()));
 	}
-	
+
+	private static List<GrantedAuthority> makeGrantedAuthority(List<MemberRole> roles) {
+		List<GrantedAuthority> list = new ArrayList<>();
+		roles.forEach(role -> list.add(new SimpleGrantedAuthority(ROLE_PREFIX + role.getRoleName())));
+		return list;
+	}
 }
