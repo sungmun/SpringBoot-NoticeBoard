26a36bbdcde13741e2ace03c281a6fd6854071fa
seungmun <tjdans174@gmail.com>
2018-10-15 20:57:45 +0900

    [#15] Fead : Config에 인증 관련 설정 추가

--- src/main/java/com/sungmun/NoticeBoard/config/SecurityConfig.java
+++ src/main/java/com/sungmun/NoticeBoard/config/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.sungmun.NoticeBoard.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import com.sungmun.NoticeBoard.service.MemberService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	MemberService service;
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/css/**", "/js/**", "/ckeditor5/**", "lib/**", "/h2-console/*");
@@ -33,5 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service).passwordEncoder(passwordEncoder());
 	}
 }
