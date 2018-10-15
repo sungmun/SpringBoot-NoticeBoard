acda5ef21ef0e12a0daae54412c8cbdb2be70313
seungmun <tjdans174@gmail.com>
2018-10-15 20:51:16 +0900

    [#15] Fead : 로그인 처리시에 UserDetails이 필요하기에 UserDetails을 반환해주는 메소드 추가

--- src/main/java/com/sungmun/NoticeBoard/service/MemberService.java
+++ src/main/java/com/sungmun/NoticeBoard/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.sungmun.NoticeBoard.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +13,16 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Service
-public class MemberService {
+public class MemberService implements UserDetailsService {
 	private MemberRepository repository;
 	
 	public String save(MemberSaveRequestDto dto) {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		dto.setPassword(passwordEncoder.encode(dto.getPassword()));
 		return repository.save(dto.toEntity()).getId();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
 	}
 }
