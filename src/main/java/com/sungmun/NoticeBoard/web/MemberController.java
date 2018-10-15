0fdc6d18b254be2e1638a61dbb3a5433de569e5d
seungmun <tjdans174@gmail.com>
2018-10-15 21:49:52 +0900

    [#15] Fead : 로그인 성공시 로그인 전페이지로 전환하기 위해 세션에 로그인 페이지 전페이지의 정보를 저장

--- src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
+++ src/main/java/com/sungmun/NoticeBoard/web/MemberController.java
@@ -1,5 +1,7 @@
 package com.sungmun.NoticeBoard.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +17,8 @@ public class MemberController {
 	MemberService service;
 
 	@GetMapping("/login")
-	public String login() {
+	public String login(HttpServletRequest request) {
+		request.getSession().setAttribute("prevPage", request.getHeader("Referer"));
 		return "member/login";
 	}
 
