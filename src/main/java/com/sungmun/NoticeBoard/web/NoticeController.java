3ed0fcffd216605e2d06809ba33daa747edacdab
seungmun <tjdans174@gmail.com>
2018-10-18 21:22:46 +0900

    [#23] Fead : Template엔진을 직접적으로 호출하여 hbs파일을 원하는 부분만 컴파일 해서 출력하는 방식으로 변경

--- src/main/java/com/sungmun/NoticeBoard/web/NoticeController.java
+++ src/main/java/com/sungmun/NoticeBoard/web/NoticeController.java
@@ -1,24 +1,52 @@
 package com.sungmun.NoticeBoard.web;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
+import com.github.jknack.handlebars.io.TemplateLoader;
+import com.sungmun.NoticeBoard.dto.comment.CommentReadResponseDto;
+import com.sungmun.NoticeBoard.service.CommentService;
 import com.sungmun.NoticeBoard.service.NoticeService;
 
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/notice")
 public class NoticeController {
-	NoticeService service;
+	NoticeService noticeService;
+	CommentService commentService;
 	
-	@GetMapping("/read")
-	public String read(Model model, @RequestParam long num) {
-		model.addAttribute("notice", service.findById(num));
-		return "notice/read";
-	}
+	@GetMapping("/notice/read")
+	@ResponseBody
+	public String read(@RequestParam long num) throws IOException {
+		TemplateLoader templateLoader = new ClassPathTemplateLoader();
+        templateLoader.setPrefix("/templates");
+        templateLoader.setSuffix(".hbs");
+ 
+        Handlebars handlebarsEngine = new Handlebars(templateLoader);
+ 
+        Template view = handlebarsEngine.compile("notice/readLayout");
+        Template InfoLayer = handlebarsEngine.compile("notice/read");
+        Template commentLayer = handlebarsEngine.compile("notice/comment/read");
+        Template topLayer=handlebarsEngine.compile("common/topbar");
+        
+        Map<String, String> model = new HashMap<String, String>();
+        model.put("commentTemplate", commentLayer.text());
+        model.put("topbar", topLayer.apply(""));
+        model.put("notice", InfoLayer.apply(noticeService.findById(num)));
+        model.put("readComment",commentLayer.apply(commentService.findByid(num)));
+        return view.apply(model);
+	}	
+	
 }
