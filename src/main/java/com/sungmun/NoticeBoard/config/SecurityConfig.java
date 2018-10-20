a41c6b041e8d949a0f292635fcca1d36bde5e01b
seungmun <tjdans174@gmail.com>
2018-10-20 17:14:13 +0900

    Fead : Admin전용 페이지가 없기 때문에 admin관련 설정 제거

--- src/main/java/com/sungmun/NoticeBoard/config/SecurityConfig.java
+++ src/main/java/com/sungmun/NoticeBoard/config/SecurityConfig.java
@@ -28,8 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/admin/**")
-			.hasRole("ADMIN").antMatchers("/**").permitAll()
 		.and()
 			.authorizeRequests()
 			.antMatchers("/member/logout","**/update","**/write")
