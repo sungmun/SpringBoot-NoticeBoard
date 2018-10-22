98e0c3f8057148d4c00c9bb101225a0748f3ce6c
seungmun <tjdans174@gmail.com>
2018-10-22 20:03:23 +0900

    Fead : DB를 mysql로 변경하였으며, DB에 들어가는 정보는 다른 파일에 넣고 커밋에서 제외한다

--- src/main/java/com/sungmun/NoticeBoard/Application.java
+++ src/main/java/com/sungmun/NoticeBoard/Application.java
@@ -1,14 +1,19 @@
 package com.sungmun.NoticeBoard;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "C:/Users/seungmun/Desktop/Study/Programing/Language/Spring/NoticeBoard/src/main/resources/privateConfig.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 }
